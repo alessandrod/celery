379f15e31e9b6e08148ef5c889404cf0cab80aa0
Alessandro Decina <alessandro.d@gmail.com>
2011-11-01 12:13:36 +0100

    Fix instantiating the app if --app is specified on the command line

--- celery/bin/base.py
+++ celery/bin/base.py
@@ -163,7 +163,7 @@ class Command(object):
         if config_module:
             os.environ["CELERY_CONFIG_MODULE"] = config_module
         if app:
-            self.app = self.get_cls_by_name(app)
+            self.app = self.get_cls_by_name(app)(loader=loader)
         else:
             self.app = self.get_app(loader=loader)
         if self.enable_config_from_cmdline:
